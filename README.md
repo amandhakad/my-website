f2277a0ff4784a5d4705559800913a23ad26b989
Aman Dhakad <dhakadaman6@gmail.com>
2020-04-19 11:54:51 +0530

    Blog Posts Model Created & Registered in Admin
    Migrations made!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Rebuilding my personal website (blog) using Django in Python.
 
 Development demo: http://v2.amandhakad.com
+
+> Hosted on GCP instance [Ubuntu 18.04](https://www.ubuntu.com/) using [Gunicorn](https://gunicorn.org/) and [NGINX](http://nginx.org/).
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    post_content = models.TextField()
+    date_published = models.DateTimeField('date published')
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date_published']
+
+    def __str__(self):
+        return self.title
